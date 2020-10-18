da06a136ae217b58d753aa9170290c3d68d11022
hritu567 <hritumog96@gmail.com>
2020-10-18 21:06:10 +0530

    Added : validation check

--- email.sh
+++ email.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 read -p "enter the email " mail
-pat='^[a-zA-Z0-9]+@[a-zA-Z]+.[a-z]{2,3}$'
+pat='^[a-zA-Z0-9]+([.@#$%&+_][a-zA-Z0-9]+)*@[a-zA-Z]+.[a-z]{2,3}$'
 if [[ $pat =~ $mail ]]
 then
  echo "yes"
