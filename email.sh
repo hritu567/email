191eab03a20a51ae5f1d2466cdaa914890bd9088
hritu567 <hritumog96@gmail.com>
2020-10-18 21:08:39 +0530

    Added : optional part

--- email.sh
+++ email.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 read -p "enter the email " mail
-pat='^[a-zA-Z0-9]+([.@#$%&+_][a-zA-Z0-9]+)*@[a-zA-Z]+.[a-z]{2,3}$'
-if [[ $pat =~ $mail ]]
+pat='^[a-zA-Z0-9]+([.@#$%&+_][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,3}([.][a-z]+)*$'
+if [[ $mail =~ $pat ]]
 then
  echo "yes"
 else
